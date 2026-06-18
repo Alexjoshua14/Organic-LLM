7fe2d1aeb60bf06ca8498587d374a5ad1e2e90dc
Alex Joshua <ajoshua14@icloud.com>
2026-06-17 20:38:54 -0700

    Add dev and update packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ["aetherion.local"],
+    allowedDevOrigins: [
+	"aetherion.local",
+	"192.168.4.31",
+    ],
   transpilePackages: ["@organic-llm/morph-physics"],
   experimental: {
     optimizePackageImports: ["lucide-react", "framer-motion", "@radix-ui/react-icons"],
